1ae04cdda4312a611fba39b3385066454fe5ed25
thanh <vtp5334@gmail.com>
2023-07-05 11:28:03 +0200

    chao

--- src/index.js
+++ src/index.js
@@ -14,10 +14,10 @@ app.engine(
         extname: 'hbs',
     }),
 );
-app.set('view engine', 'hbs');
+  app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => res.render('home'));
+        app.get('/', (req, res) => res.render('home'));
 app.get('/search', (req, res) => res.render('search'));
 app.get('/news', (req, res) => res.render('news'));
 app.get('/news/:slug', (req, res) => res.render('detail'));
