bf9b22350d80f19b8ad82af802c3c04dca719b91
thanh <vtp5334@gmail.com>
2023-07-11 16:51:36 +0200

    first nodejs

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 
 async function connect(){
     try {
+        /* await mongoose.connect('mongodb+srv://vtp5339:thanhphan76@cluster0.my7p3yr.mongodb.net/nodejs' */
         await mongoose.connect('mongodb+srv://vtp5339:thanhphan76@cluster0.my7p3yr.mongodb.net/nodejs',{
             useNewUrlParser: true,
             useUnifiedTopology: true
